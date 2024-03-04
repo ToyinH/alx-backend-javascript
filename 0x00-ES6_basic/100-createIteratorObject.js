62337e056a0a84db900a6f83c11c02f8a8bfe4ff
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-03-04 22:21:09 +0100

    Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,17 @@
+export default function createIteratorObject(report) {
+    function* employeeIterator(report) {
+      for (const department in report.allEmployees) {
+        const employees = report.allEmployees[department];
+        for (const employee of employees) {
+          yield employee;
+        }
+      }
+    }
+  
+    return {
+      [Symbol.iterator]() {
+        return employeeIterator(report);
+      }
+    };
+  }
+  
