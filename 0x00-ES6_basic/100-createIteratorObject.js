c493e9a8fb744d2dda5d3b7f5ff91db5416154b1
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-03-04 22:28:47 +0100

    update

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,17 +1,16 @@
 export default function createIteratorObject(report) {
-    function* employeeIterator(report) {
-      for (const department in report.allEmployees) {
-        const employees = report.allEmployees[department];
-        for (const employee of employees) {
-          yield employee;
-        }
+  function* employeeIterator(report) {
+    for (const department in report.allEmployees) {
+      const employees = report.allEmployees[department];
+      for (const employee of employees) {
+        yield employee;
       }
     }
-  
-    return {
-      [Symbol.iterator]() {
-        return employeeIterator(report);
-      }
-    };
   }
-  
+
+  return {
+    [Symbol.iterator]() {
+      return employeeIterator(report);
+    },
+  };
+}
