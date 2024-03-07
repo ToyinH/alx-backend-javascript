050d4e493ccabea932e7ebdff3198f420eb997b9
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-03-07 03:50:18 +0100

    Append three handlers to the function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(response => {
+  // eslint-disable-next-line no-unused-vars
+    .then((response) => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .catch(error => {
+    .catch((error) => {
       console.error('Error:', error.message);
       return new Error(); // Return an empty Error object
     });
