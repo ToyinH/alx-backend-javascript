5f1e97854077363f9d79eb0e9a6a1ad38779b970
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-03-12 22:13:43 +0100

    update

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -3,13 +3,9 @@ export default function updateUniqueItems(map) {
     throw new Error('Cannot process');
   }
 
-  const updatedMap = new Map();
   map.forEach((value, key) => {
     if (value === 1) {
-      updatedMap.set(key, 100);
-    } else {
-      updatedMap.set(key, value);
+      map.set(key, 100);
     }
   });
-  return updatedMap;
 }
