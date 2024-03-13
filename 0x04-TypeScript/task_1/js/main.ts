01d45cf23b6ecdb1a98929591270f9892d318c65
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-03-13 21:03:05 +0100

    update

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,52 @@ function createTeacher(firstName: string, lastName: string, fullTimeEmployee: bo
     ...additionalProps // Spread operator to add any additional properties
   };
 }
+
+// Define the Directors interface that extends Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Define the interface for the printTeacher function
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Define the printTeacher function
+const printTeacher: PrintTeacherFunction = (firstName: string, lastName: string): string => {
+  const firstLetter = firstName.charAt(0).toUpperCase();
+  const fullName = `${firstLetter}. ${lastName}`;
+  return fullName;
+};
+
+// Define interface for the constructor parameters
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+// Define interface for the Student class
+interface Student {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Implement the Student interface in StudentClass
+class StudentClass implements Student {
+  private firstName: string;
+  private lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
