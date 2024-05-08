91d14a95eac448ec4db89f6ceac17ecc9035e0ee
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-05-08 07:23:47 +0100

    print to stdout

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,12 @@
+// 0-console.js
+
+/**
+ * Function to display a message in STDOUT.
+ * @param {string} message - The message to be displayed.
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+// Export the function
+module.exports = displayMessage;
